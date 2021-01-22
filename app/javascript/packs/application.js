6fc0807c0be8ddcfe6f3d4c4cdda21aa957ce30f
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-22 16:30:55 +0900

    デプロイ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start() ←javascriptを正常に読み込ませるために削除
 require("@rails/activestorage").start()
 require("channels")
 
