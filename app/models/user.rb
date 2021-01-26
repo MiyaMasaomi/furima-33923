0d685a58357ea9042618f0291a742d87f4b1399e
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-26 16:03:45 +0900

    エラーハンドリングの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  #birth_idはactivehashで採用するため、必要なし
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_id, presence: true
        
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/ }
        
   #has_many :items
   #has_many :purchase_records
