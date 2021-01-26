66f07afd7b93ef98af17588b5363cfa5349b2377
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-26 11:30:36 +0900

    deviseのアクションに限定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
   private
@@ -9,4 +10,8 @@ class ApplicationController < ActionController::Base
     end
   end
   #上記、basic認証のため記述
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:first_name_kana,:last_name,:last_name_kana,:birth_id])
+  end
 end
