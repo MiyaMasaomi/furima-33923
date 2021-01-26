c2b446223270bfe772d8a033f7cda19340b18e04
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-26 11:20:23 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  #birth_idはactivehashで採用するため、必要なし
+       
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+       
+  #has_many :items
+  #has_many :purchase_records
+  
 end
