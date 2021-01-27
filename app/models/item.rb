4800ea2600298fb20bc075e3a19eb8136e71aab2
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-27 14:39:16 +0900

    ActiveHashの導入・バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition_type
+  belongs_to :postage
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
   belongs_to :user
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
-    validates :category_id
-    validates :condition_type_id
-    validates :postage_id
-    validates :shipping_area_id
-    validates :days_to_ship_id
+    validates :price, format: { with: /\A[0-9]+\z/ }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_type_id
+      validates :postage_id
+      validates :shipping_area_id
+      validates :days_to_ship_id
+    end
   end
 end
