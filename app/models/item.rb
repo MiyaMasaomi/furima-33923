970a802ba42a0a3504722003d88c405c81cc7860
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-27 12:31:54 +0900

    itemsテーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_type_id
+    validates :postage_id
+    validates :shipping_area_id
+    validates :days_to_ship_id
+  end
+end
