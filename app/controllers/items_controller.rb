584bb57bf513df86d821c299f2c27a6f8a3e58c2
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-28 12:29:59 +0900

    image修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:description,:price,:category_id,:condition_type_id,:postage_id,:shipping_area_id,:days_to_ship_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name,:description,:price,:category_id,:condition_type_id,:postage_id,:shipping_area_id,:days_to_ship_id).merge(user_id: current_user.id)
   end
 end
