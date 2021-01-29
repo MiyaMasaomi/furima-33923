709d390a51ece6e72cdc874395925a0b66a06536
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-01-29 19:05:08 +0900

    削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   #indexのルーティング記述後、controller items indexでコントローラー作成
-  resources :items, only: [:index, :new, :create, :show, :edit,:update]
+  resources :items
 end
