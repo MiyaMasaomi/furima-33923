bc44578934331e3e3a93190e912d0d169df04d41
MiyaMasaomi <masaomi0223@pandapharmacist.com>
2021-02-01 11:50:11 +0900

    Formオブジェクトの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,5 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Please set including both letters and numbers'
 
-  # has_many :items
   # has_many :purchase_records
 end
